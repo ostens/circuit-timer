b6ecafa09da626b0ea5e4acef19406c49bdf40e4
ostens <erinjayaustin@gmail.com>
2020-06-19 17:55:56 +0100

    Improve time conversion

--- src/Components/StyledComponents/Timer/TimerUtils.js
+++ src/Components/StyledComponents/Timer/TimerUtils.js
@@ -1,5 +1,10 @@
 export const convertTime = (totalSeconds) => {
   const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60 || "00";
-  return(`${minutes}:${seconds}`);
+  const convertSeconds = (totalSeconds) => {
+    if (totalSeconds % 60 < 10) {
+      return `0${totalSeconds % 60}`;
+    }
+    return totalSeconds % 60;
+  }
+  return totalSeconds < 0 ? "0:00" : `${minutes}:${convertSeconds(totalSeconds)}`;
 }
