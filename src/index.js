977427bcb0c924d4e1deacc865ba7cf840f97100
ostens <erinjayaustin@gmail.com>
2020-06-25 18:03:12 +0100

    Add timer page and root reducer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import rootReducer from "./reducers/TimerForm";
+import rootReducer from "./reducers/rootReducer";
 import App from "./components/App/App";
 import * as serviceWorker from './serviceWorker';
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 
 import timerForm from "./TimerForm";
+import timerPage from "./TimerPage";
 
-const rootReducer = () =>
-  combineReducers({
+const rootReducer = combineReducers({
     timerForm,
+    timerPage
   });
 
 export default rootReducer;
