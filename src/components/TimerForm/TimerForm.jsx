4b2cbad9e972a5dfff57ed60a16f3d8ed1db9f5e
ostens <erinjayaustin@gmail.com>
2020-07-09 17:53:34 +0100

    Remove import

--- src/components/TimerForm/TimerForm.jsx
+++ src/components/TimerForm/TimerForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import "./TimerForm.scss";
 import Button from "../StyledComponents/Button/Button";
 import InputTextCard from "../StyledComponents/InputTextCard/InputTextCard";
 import InputNumberCard from "../StyledComponents/InputNumberCard/InputNumberCard";
