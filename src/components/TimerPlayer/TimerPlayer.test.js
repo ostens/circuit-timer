9011c936cb84a7079c24efbb5d11f09b23a70850
ostens <erinjayaustin@gmail.com>
2020-07-20 15:34:01 +0100

    Add first component unit test

--- src/components/TimerPlayer/TimerPlayer.test.js
+++ src/components/TimerPlayer/TimerPlayer.test.js
@@ -1,4 +1,28 @@
-import { calculateTotalTime, convertTime } from './TimerPlayerUtils';
+import React from "react";
+import { Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+
+import TimerPlayer from "./TimerPlayer";
+import { calculateTotalTime, convertTime } from "./TimerPlayerUtils";
+
+const history = createMemoryHistory();
+
+const defaultTimer = {
+  timerName: "Long default",
+  activeTime: 10,
+  restTime: 5,
+  intervals: 10
+};
+
+it("renders initial state of timer player", () => {
+  render(
+    <Router history={history}>
+      <TimerPlayer timer={defaultTimer} deleteTimer={() => { }} />
+    </Router>
+  );
+  expect(screen.getByText("Active")).toBeInTheDocument();
+});
 
 it("calcuates the total seconds from the times and intervals", () => {
   expect(calculateTotalTime(35, 15, 8)).toEqual(400);
